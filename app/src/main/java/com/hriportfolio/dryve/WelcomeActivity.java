4f3aa6ace91c5fa8ffe61b8ef9855612cecabf71
Tawhidis007 <35430163+Tawhidis007@users.noreply.github.com>
2020-04-27 03:27:42 +0600

    haha

--- app/src/main/java/com/hriportfolio/dryve/WelcomeActivity.java
+++ app/src/main/java/com/hriportfolio/dryve/WelcomeActivity.java
@@ -14,6 +14,7 @@ import com.hriportfolio.dryve.Driver.DriverLoginActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
